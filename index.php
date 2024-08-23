b3671f08944e04749cb3069709b96039250184ef
augustodlsr <augustodlsr@gmail.com>
2024-08-23 18:38:26 -0300

    filtro

--- index.php
+++ index.php
@@ -9,7 +9,9 @@
 </head>
 
 <body>
-
+    
+    <?php require("header.php");
+    ?>
 
     <div class="content">
 
@@ -39,9 +41,9 @@
                 <option value="3">Femeninas</option>
               </select>
           </div>
-          
-
     </div>
+
+
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
