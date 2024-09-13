60a7d8e018f8683655bf933aa3937fa123a7f3c4
AlejoREPAY <alejo011106@gmail.com>
2024-09-13 18:56:51 -0300

    footer2

--- footer.php
+++ footer.php
@@ -12,6 +12,12 @@
     </head>
     <body>
     <footer class="footer1">
+        <div class="waves">
+            <div class="wave" id="wave1"></div>
+            <div class="wave" id="wave2"></div>
+            <div class="wave" id="wave3"></div>
+            <div class="wave" id="wave4"></div>
+        </div>
         <ul class="social-icon">
             <li><a href="#"><ion-icon name="logo-instagram"></ion-icon></a></li>
             <li><a href="#"><ion-icon name="logo-instagram"></ion-icon></a></li>
@@ -27,6 +33,7 @@
             <li><a href="#">Home</a></li>
             <li><a href="#">Home</a></li>
         </ul>
+        <p>©2024 Residencias Estudiantiles | Todos los derechos reservados</p>
     </footer>
 
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
