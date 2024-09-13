c8af190b9538be3ba1b48bb1367a360edaec83fc
AlejoREPAY <alejo011106@gmail.com>
2024-09-13 17:24:36 -0300

    footer

--- header.php
+++ header.php
@@ -6,6 +6,7 @@
   
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css">
     <link rel="stylesheet" href="estilo.css">
     <title>Document</title>
 </head>
@@ -20,6 +21,14 @@
         <label for="menu">
             <img src="resi.jpg" class="menu-icono">
         </label>
+        <div class="wrapper">
+            <input type="checkbox" id="btn" hidden>
+            <label for="btn" class="menu-btn">
+            <i class="fas fa-bars"></i>
+            <i class="fas fa-times"></i>
+
+            </label>
+        </div>
     
     <nav class="navbar">
         
