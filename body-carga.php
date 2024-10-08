cd44634f69d93ad898e112b506c3c0f5a3ff43fa
AlejoREPAY <alejo011106@gmail.com>
2024-10-08 15:57:05 -0300

    arreglosCarga

--- body-carga.php
+++ body-carga.php
@@ -3,16 +3,57 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="estilo.css">
+  
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css">
-    <title>Cargar Residencia</title>
+    <link rel="stylesheet" href="estilo.css">
+    <title>Document</title>
 </head>
+
 <body>
+
+   <header class="header">
+
+    <div class="menu container">
+        <img src="http://localhost/conexionPHPMysql-main/images/logoblahd.png" alt="">
+     
+        <input type="checkbox" id="menu">
+        <label for="menu">
+            <img src="resi.jpg" class="menu-icono">
+        </label>
+        <div class="wrapper">
+            <input type="checkbox" id="btn" hidden>
+            <label for="btn" class="menu-btn">
+            <i class="fas fa-bars"></i>
+            <i class="fas fa-times"></i>
+
+            </label>
+        </div>
+    
+    <nav class="navbar">
+        
+        <ul>
+            <li class="nav-link">
+                <a href="#">Inicio</a>
+                
+            </li>
+            <li class="nav-link">
+                <a href="#">Menu</a>
+            </li>
+            <li class="nav-link">
+                <a href="iniregi.php">Iniciar Sesión</a>
+            </li>
+        </ul>
+    </nav> 
+   
+    </div>
+
+
 <?php require("header-carga.php");
     ?>
-    <div class="padrino">
+
+   
         <form action="RF_registro_resi.php" method="POST" id="envio">
         <div class="card_resi">
         <div>
@@ -62,6 +103,14 @@
         <script src="app.js"></script>
         
     </div>
-    </div>
-</body>
+    
 </html>
+    
+</body>
+
+ 
+</div>
+
+</header>
+<?php require("footer.php");
+    ?>
