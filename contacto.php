fd1c3b317ae9e2bf2e21f0f4b58fb5f0842a4907
AlejoREPAY <alejo011106@gmail.com>
2024-10-11 18:48:09 -0300

    acerca de

---
+++ contacto.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="estilo.css">
+</head>
+<body class="body2">
+    <div class="container2">
+        <div class="box-info">
+            <h1>CARGA TU RESIDENCIA</h1>
+            <div class="data">
+                <p><i class="fa-solid fa-phone"></i>+598 96 626 103</p>
+                <p><i class="fa-solid fa-envelope"></i>repayailea2024@gmail.com</p>
+                <p><i class="fa-solid fa-location-dot"></i>Av.España entre Felippone y Juncal. PAYSANDÚ</p>
+            </div>
+            <div class="links">
+                <a href="#"><i class="fa-brands fa-instagram"></i></a>
+            </div>
+        </div>
+        <form class="form2">
+            <div class="input-box">
+                <input type="text" required placeholder="Nombre y Apellido">
+                <i class="fa-solid fa-user"></i>
+            </div>
+            <div class="input-box">
+                <input type="email" required placeholder="Correo electronico">
+                <i class="fa-solid fa-pen-to-square"></i>
+
+            </div>
+            <div class="input-box">
+                <input type="text" required placeholder="Asunto">
+                <i class="fa-solid fa-user"></i>
+
+            </div>
+            <div class="input-box">
+                <textarea cols="30" rows="10" placeholder="Escribe tu mensaje"></textarea>
+            </div>
+                <button type="submit">Envir mensaje</button>
+        </form>
+    </div>
+
+
+
+
+
+
+</body>
+</html>
