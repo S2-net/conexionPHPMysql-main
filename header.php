cd21b4ab6c7af4e335cd0bd546717f136e84735b
augustodlsr <augustodlsr@gmail.com>
2024-10-10 23:23:50 -0300

    responsive

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
   
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <link rel="stylesheet" href="estilo.css">
     <title>Document</title>
 </head>
@@ -14,40 +14,34 @@
 <body>
    <header class="header">
 
-    <div class="menu container">
-        <img src="http://localhost/conexionPHPMysql-main/images/logoblahd.png" alt="">
-     
-        <input type="checkbox" id="menu">
-        <label for="menu">
-            <img src="resi.jpg" class="menu-icono">
-        </label>
-        <div class="wrapper">
-            <input type="checkbox" id="btn" hidden>
-            <label for="btn" class="menu-btn">
-            <i class="fas fa-bars"></i>
-            <i class="fas fa-times"></i>
+    <div class="menu">
+    
+        <nav class="navbar">
 
-            </label>
-        </div>
-    
-    <nav class="navbar">
-        
-        <ul>
-            <li class="nav-link">
-                <a href="#">Inicio</a>
-                
-            </li>
-            <li class="nav-link">
+        <input type="checkbox" id="check">
+        <label for="check" class="checkbtn">
+            <i class="fa-solid fa-list-ul"></i>
+        </label>
+
+        <a href="#">
+            <img src="http://localhost/conexionPHPMysql-main/images/logoblahd.png" alt="" class="logo">
+        </a>
+
+            <ul>
+              <li class="nav-link">
+                <a href="#">Inicio</a>   
+              </li>
+              <li class="nav-link">
                 <a href="#">Menu</a>
-            </li>
-            <li class="nav-link">
+              </li>
+              <li class="nav-link">
                 <a href="iniregi.php">Iniciar Sesión</a>
-            </li>
-        </ul>
-    </nav>
-    
+              </li>
+            </ul>
+        </nav>  
+        <section></section>
  
-</div>
+    </div>
 
     <div class="header-content container">
 
