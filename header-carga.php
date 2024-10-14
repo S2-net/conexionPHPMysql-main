dca55958625ac6cee13ce0ecde93d349d3f17b0b
AlejoREPAY <alejo011106@gmail.com>
2024-10-13 23:16:16 -0300

    responsive

--- header-carga.php
+++ header-carga.php
@@ -1,4 +1,4 @@
- <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -6,52 +6,39 @@
   
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <link rel="stylesheet" href="estilo.css">
     <title>Document</title>
 </head>
 
 <body>
-<header class="header">
+   <header class="header">
 
-<div class="menu container">
-    <img src="http://localhost/conexionPHPMysql-main/images/logoblahd.png" alt="">
- 
-    <input type="checkbox" id="menu">
-    <label for="menu">
-        <img src="resi.jpg" class="menu-icono">
-    </label>
-    <div class="wrapper">
-        <input type="checkbox" id="btn" hidden>
-        <label for="btn" class="menu-btn">
-        <i class="fas fa-bars"></i>
-        <i class="fas fa-times"></i>
-
+    <div class="menu container">
+    <a href="#">
+            <img src="http://localhost/conexionPHPMysql-main/images/logoblahd.png" alt="" class="logo">
+        </a>
+        <input type="checkbox" id="menu">
+        <label for="menu">
+      <img src="http://localhost/conexionPHPMysql-main/images/menu.png" class="menu-icono" alt="">
         </label>
-    </div>
+        <nav class="navbar">
+       
 
-<nav class="navbar">
-    
-    <ul>
-        <li class="nav-link">
-            <a href="index-propietario.php">Inicio</a>
-            
-        </li>
-      
-    </li>
-        <li class="nav-link">
-            <a href="perfilusuario.php">Ver perfil</a>
-    </li>
-    </li>
-        <li class="nav-link">
-            <a href="carga.php">Cargar Residencia</a>
-    </li>
-    <li class="nav-link">
-            <a href="index.php">Cerrar Sesion</a>
-    </li>
-    </ul>
-</nav>
-</div>
+            <ul>
+              <li class="nav-link">
+                <a href="#">Inicio</a>   
+              </li>
+              <li class="nav-link">
+                <a href="#">Menu</a>
+              </li>
+              <li class="nav-link">
+                <a href="iniregi.php">Cerrar Sesión</a>
+              </li>
+            </ul>
+        </nav>  
+ 
+    </div>
 
 </header>
 <div class="datos">
