ee64a4053643a919750fea76e5d3b29297dd5836
augustodlsr <augustodlsr@gmail.com>
2024-10-16 18:28:28 -0300

    claroo

--- header-usuario.php
+++ header-usuario.php
@@ -6,7 +6,7 @@
   
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <link rel="stylesheet" href="estilo.css">
     <title>Document</title>
 </head>
@@ -15,41 +15,33 @@
    <header class="header">
 
     <div class="menu container">
-        <img src="http://localhost/conexionPHPMysql-main/images/logoblahd.png" alt="">
-     
+    <a href="#">
+            <img src="http://localhost/conexionPHPMysql-main/images/logoblahd.png" alt="" class="logo">
+        </a>
         <input type="checkbox" id="menu">
         <label for="menu">
-            <img src="resi.jpg" class="menu-icono">
+      <img src="http://localhost/conexionPHPMysql-main/images/menu.png" class="menu-icono" alt="">
         </label>
-        <div class="wrapper">
-            <input type="checkbox" id="btn" hidden>
-            <label for="btn" class="menu-btn">
-            <i class="fas fa-bars"></i>
-            <i class="fas fa-times"></i>
+        <nav class="navbar">
+       
 
-            </label>
-        </div>
-    
-    <nav class="navbar">
-        
-        <ul>
-            <li class="nav-link">
-                <a href="index-usuario.php">Inicio</a>
-                
-            </li>
-        </li>
-            <li class="nav-link">
-                <a href="perfilusuario.php">Ver perfil</a>
-        </li>
-        </li>
-            <li class="nav-link">
-                <a href="index.php">Cerrar sesion</a>
-        </li>
-        </ul>
-    </nav>
-    
+            <ul>
+              <li class="nav-link">
+                <a href="#">Inicio</a>   
+              </li>
+              <li class="nav-link">
+                <a href="perfil-propietario.php">Ver Perfil</a>   
+              </li>
+              <li class="nav-link">
+                <a href="contacto.php">Contactanos</a>
+              </li>
+              <li class="nav-link">
+                <a href="index.php">Cerrar Sesion</a>
+              </li>
+            </ul>
+        </nav>  
  
-</div>
+    </div>
 
     <div class="header-content container">
 
