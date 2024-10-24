ddc78332526fdb9254a0f2812fc14685d4fbe761
eliasfifi <mesialeon1@gmail.com>
2024-10-24 15:37:18 -0300

    mostrarcontra

--- iniregi.js
+++ iniregi.js
@@ -9,3 +9,21 @@ registerBtn.addEventListener('click', () => {
 loginBtn.addEventListener('click', () => {
     containerIR.classList.remove("active");
 });
+
+/* Mostrar/Ocultar contraseña en inicio de sesión */
+var contador = true;
+
+function vista() {
+    var texto = document.getElementById("verPassword");
+    var input = document.getElementById("input");
+
+    if (contador === true) {
+        texto.className = "fas fa-eye-slash verPassword";
+        input.type = "text";
+        contador = false;
+    } else {
+        texto.className = "fas fa-eye verPassword";
+        input.type = "password";
+        contador = true;
+    }
+}
