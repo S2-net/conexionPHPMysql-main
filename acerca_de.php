bfe258dad925cb0dae1499351501bf8cff059c1f
AlejoREPAY <alejo011106@gmail.com>
2024-10-25 18:27:30 -0300

    acercade

---
+++ acerca_de.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="estilo.css">
+</head>
+<body class="acerca">
+        <div class="about-section">
+            <div class="inner-container">
+                <div class="fondologo">
+                <img src="http://localhost/conexionPHPMysql-main/images/logosinfondohd1.png" alt="">
+                </div>
+                <h1>ACERCA DE NOSOTROS</h1>
+                <p class="text1">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur sed blanditiis ducimus ipsa, porro recusandae in obcaecati quia dolorum dolor error nemo! Maiores dignissimos dicta inventore tempora ea, voluptatem vero!
+                </p>
+                <div class="skills">
+                    <span>ITSP</span>
+                    <span>Estudiantes</span>
+                    <span>Paysandú</span>
+                </div>
+            </div>
+        </div>
+
+
+
+
+</body>
+</html>
