1c737f7dc2fc9a2945921b86f2f52bdc9b47ac65
REPAY-AILEA <repayailea2024@gmail.com>
2024-10-29 09:03:24 -0300

    css filtro

--- header.php
+++ header.php
@@ -43,7 +43,8 @@
  
     </div>
 
-    <div class="header-content container">
+    <div class="container-filtro">
+    <div class="header-content">
         <div class="header-txt">
             <h1>Encuentra tu residencia ideal para el exito academico</h1>
         </div>
@@ -73,13 +74,14 @@
                         </select>
                       
                     </div>
-                  
-                </div>
-                <div class="col-md-12">
+                    <div class="col-md-3">
                         <input type="submit" value="Buscar" class="btn btn-primary mt-3">
                     </div>
+                </div>
+
             </form>
         </div>
+    </div>
     </div>
    </header> 
 </body>
